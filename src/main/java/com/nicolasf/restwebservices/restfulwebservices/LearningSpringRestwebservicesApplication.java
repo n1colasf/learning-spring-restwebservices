fe6986d5df2a235e6bb2b3504f7aecb8f547159f
Nicolas <nicolas@nicolasf.uy>
2023-08-25 16:37:27 -0300

    Start section 8

---
+++ src/main/java/com/nicolasf/restwebservices/restfulwebservices/LearningSpringRestwebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.nicolasf.restwebservices.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringRestwebservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningSpringRestwebservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nicolasf/restwebservices/restfulwebservices/LearningSpringRestwebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolasf.restwebservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringRestwebservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
